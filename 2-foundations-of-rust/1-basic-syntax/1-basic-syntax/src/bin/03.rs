7ba8342f6cf608b96fb4c6ae9801264183432ce8
robertpetcu <151735719+robertpetcu@users.noreply.github.com>
2024-10-09 16:27:25 +0000

    first lab

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,8 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    let largest = input.iter().max().unwrap_or(&0);
+    let smallest = input.iter().min().unwrap_or(&0);
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", largest, smallest);
 }
